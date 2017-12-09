b882908e99c97a2e6b462721583a4ab8bd20fed3
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-09 12:05:50 -0500

    cleaned up

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-from algorithm import N_BATCH, N_STATES, EPISODES, MAX_TIMESTEPS, act, step, \
-    update, REWARDS
-
 from matplotlib import animation
+from algorithm import N_BATCH, N_STATES, act, step, \
+    update
 
 fig = plt.figure()
 ax = plt.axes()
 
 value_matrix = np.zeros((N_BATCH, N_STATES), dtype=np.float)
+im = plt.imshow(value_matrix, vmin=0, vmax=1, animated=True)
 states = np.random.choice(N_STATES, N_BATCH)
-
 circles = []
 for i, state in enumerate(states):
     circle = plt.Circle((state, i), radius=0.2, color='white')
     circles.append(circle)
     ax.add_patch(circle)
 
-im = plt.imshow(value_matrix, vmin=0, vmax=1, animated=True)
-
-
 def updatefig(_):
-    global states, value_matrix, im, circles
+    global states, value_matrix
     actions = act(states, value_matrix)
     next_states, reward = step(actions, states)
     value_matrix = update(value_matrix, states, next_states)

47776d53ad15237d061e0d03da6f914c2ec5e5bc
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-09 15:00:32 -0500

    pulled Agent out of Sim

--- algorithm.py
+++ algorithm.py
@@ -12,20 +12,71 @@ def softmax(array, axis=None):
     return np.exp(array - denominator)
 
 
-class Sim:
+class Agent:
     def __init__(self, gamma, alpha, n_states, n_batch, n_actions,
                  transitions, rewards, episodes, max_timesteps):
         self.gamma = gamma
         self.alpha = alpha
-        self.n_states = n_states
-        self.n_batch = n_batch
-        self.n_actions = n_actions
-        self.transitions = transitions
-        self.rewards = rewards
         self.episodes = episodes
         self.max_timesteps = max_timesteps
         self.states = np.random.choice(n_states, n_batch)
         self.value_matrix = np.zeros((n_batch, n_states))
+        self.sim = Sim(n_states=n_states,
+                       n_actions=n_actions,
+                       n_batch=n_batch,
+                       transitions=transitions,
+                       rewards=rewards)
+
+    def act(self, states, value_matrix):
+        n_batch, = states.shape
+        assert self.sim.transitions.shape == (
+            self.sim.n_actions, self.sim.n_states, self.sim.n_states), \
+            self.sim.transitions.shape
+        assert value_matrix.shape == (n_batch, self.sim.n_states)
+
+        meshgrid = np.meshgrid(range(self.sim.n_actions), states)
+        assert len(meshgrid) == 2
+        for grid in meshgrid:
+            assert grid.shape == (n_batch, self.sim.n_actions)
+
+        next_states = self.sim.transitions[meshgrid]
+        assert next_states.shape == (
+        n_batch, self.sim.n_actions, self.sim.n_states)
+
+        transposed_value_matrix = np.expand_dims(value_matrix, 2)
+        assert transposed_value_matrix.shape == (n_batch, self.sim.n_states, 1)
+
+        next_values = np.matmul(next_states, transposed_value_matrix)
+        assert next_values.shape == (n_batch, self.sim.n_actions, 1)
+
+        actions = np.argmax(next_values, axis=1).flatten()
+        assert actions.shape == (n_batch,)
+        return actions
+
+    def update(self, value_matrix, states, next_states):
+        n_batch, = states.shape
+        assert value_matrix.shape == (n_batch, self.sim.n_states)
+        assert next_states.shape == (n_batch,)
+
+        rewards = self.sim.rewards[range(self.sim.n_batch), states]
+        assert rewards.shape == states.shape
+
+        next_values = value_matrix[range(n_batch), next_states]
+        assert next_values.shape == states.shape
+
+        value_matrix *= self.alpha
+        value_matrix[range(n_batch), states] += (1 - self.alpha) * (
+            rewards + next_values)
+        return value_matrix
+
+
+class Sim:
+    def __init__(self, n_states, n_actions, n_batch, transitions, rewards):
+        self.n_states = n_states
+        self.n_actions = n_actions
+        self.n_batch = n_batch
+        self.transitions = transitions
+        self.rewards = rewards
 
     def step(self, actions, states):
         n_batch, = actions.shape
@@ -43,46 +94,6 @@ class Sim:
 
         return next_states, rewards
 
-    def act(self, states, value_matrix):
-        n_batch, = states.shape
-        assert self.transitions.shape == (self.n_actions, self.n_states, self.n_states), \
-            self.transitions.shape
-        assert value_matrix.shape == (n_batch, self.n_states)
-
-        meshgrid = np.meshgrid(range(self.n_actions), states)
-        assert len(meshgrid) == 2
-        for grid in meshgrid:
-            assert grid.shape == (n_batch, self.n_actions)
-
-        next_states = self.transitions[meshgrid]
-        assert next_states.shape == (n_batch, self.n_actions, self.n_states)
-
-        transposed_value_matrix = np.expand_dims(value_matrix, 2)
-        assert transposed_value_matrix.shape == (n_batch, self.n_states, 1)
-
-        next_values = np.matmul(next_states, transposed_value_matrix)
-        assert next_values.shape == (n_batch, self.n_actions, 1)
-
-        actions = np.argmax(next_values, axis=1).flatten()
-        assert actions.shape == (n_batch,)
-        return actions
-
-    def update(self, value_matrix, states, next_states):
-        n_batch, = states.shape
-        assert value_matrix.shape == (n_batch, self.n_states)
-        assert next_states.shape == (n_batch,)
-
-        rewards = self.rewards[range(self.n_batch), states]
-        assert rewards.shape == states.shape
-
-        next_values = value_matrix[range(n_batch), next_states]
-        assert next_values.shape == states.shape
-
-        value_matrix *= self.alpha
-        value_matrix[range(n_batch), states] += (1 - self.alpha) * (
-        rewards + next_values)
-        return value_matrix
-
 
 if __name__ == '__main__':
     n_states = n_batch = 4
@@ -91,15 +102,10 @@ if __name__ == '__main__':
     transitions = np.stack([gaussian_filter(
         np.eye(n_states)[:, np.roll(np.arange(n_states), shift)], .5)
         for shift in [-1, 1]])  # shifted and blurred I matrices
-    sim = Sim(gamma=.95,
-              alpha=.9,
-              n_states=n_states,
-              n_batch=n_batch,
+    sim = Sim(n_states=n_states,
               n_actions=2,
               transitions=transitions,
-              rewards=rewards,
-              episodes=200,
-              max_timesteps=100)
+              rewards=rewards)
 
     for _ in range(sim.episodes):
         for _ in range(sim.max_timesteps):
--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import numpy as np
 from matplotlib import animation
 from scipy.ndimage import gaussian_filter
 
-from algorithm import Sim
-
-
+from algorithm import Sim, Agent
 
 n_states = n_batch = 4
 rewards = np.zeros((n_batch, n_states))
@@ -13,15 +11,15 @@ rewards[range(n_batch), np.random.randint(n_states, size=n_batch)] = 1
 transitions = np.stack([gaussian_filter(
     np.eye(n_states)[:, np.roll(np.arange(n_states), shift)], .5)
     for shift in [-1, 1]])  # shifted and blurred I matrices
-sim = Sim(gamma=.95,
-          alpha=.9,
-          n_states=n_states,
-          n_batch=n_batch,
-          n_actions=2,
-          transitions=transitions,
-          rewards=rewards,
-          episodes=200,
-          max_timesteps=100)
+agent = Agent(gamma=.95,
+              alpha=.9,
+              n_states=n_states,
+              n_batch=n_batch,
+              n_actions=2,
+              transitions=transitions,
+              rewards=rewards,
+              episodes=200,
+              max_timesteps=100)
 
 
 def x(j):
@@ -29,36 +27,36 @@ def x(j):
 
 
 def y(i):
-    return sim.n_states - i - 1
+    return agent.sim.n_states - i - 1
 
 
 fig = plt.figure()
 ax = plt.axes()
 
 
-value_matrix = np.zeros((sim.n_batch, sim.n_states), dtype=np.float)
+value_matrix = np.zeros((agent.sim.n_batch, agent.sim.n_states), dtype=np.float)
 im = plt.imshow(value_matrix, vmin=0, vmax=1, cmap='Oranges', animated=True)
 im.set_zorder(0)
-states = np.random.choice(sim.n_states) * np.ones(sim.n_states, dtype=int)
+states = np.random.choice(agent.sim.n_states) * np.ones(agent.sim.n_states, dtype=int)
 next_states = states
 pos = states.astype(float)
 step_size = 0
 circles = []
 texts = []
-for i in range(sim.n_batch):
+for i in range(agent.sim.n_batch):
     circle = plt.Circle((states[i], y(i)), radius=0.2, color='black', zorder=1)
     circles.append(circle)
     ax.add_patch(circle)
-    for j in range(sim.n_states):
-        texts.append(ax.text(j, i, int(sim.rewards[i, j]), zorder=2))
+    for j in range(agent.sim.n_states):
+        texts.append(ax.text(j, i, int(agent.sim.rewards[i, j]), zorder=2))
 
 
 def updatefig(_):
     global pos, states, next_states, value_matrix, step_size
     if np.allclose(pos, next_states):
-        actions = sim.act(states, value_matrix)
-        next_states, reward = sim.step(actions, states)
-        value_matrix = sim.update(value_matrix, states, next_states)
+        actions = agent.act(states, value_matrix)
+        next_states, reward = agent.sim.step(actions, states)
+        value_matrix = agent.update(value_matrix, states, next_states)
         step_size = (next_states - states) / 20
         states = next_states
         im.set_array(value_matrix)

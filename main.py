23aa63c3a8deaa100948aaf40267a0a3c9ec8150
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-10 11:24:12 -0500

    made step not return state

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import algorithm
 
 if __name__ == '__main__':
     agent1, value_matrix1, states1, next_states1 = algorithm.init()
-    agent2, value_matrix2, states2, next_states2 = algorithm.init()
+    # agent2, value_matrix2, states2, next_states2 = algorithm.init()
+
 
     def x(j):
         return j
@@ -20,33 +21,32 @@ if __name__ == '__main__':
 
     fig, (ax1, ax2) = plt.subplots(2)
 
-    for ax, agent, value_matrix, states, next_states in (
-            [[ax1, agent1, value_matrix1, states1, next_states1]]):
-            # [ax2, agent2, value_matrix2, states2, next_states2]):
-        im = ax.imshow(value_matrix, vmin=0, vmax=1, cmap='Oranges', animated=True)
-        im.set_zorder(0)
-        pos = states.astype(float)
-        step_size = 0
-        circles = []
-        texts = []
-        for i in range(agent.n_batch):
-            color = 'black' if i == 0 else 'gray'
-            circle = plt.Circle((states[i], y(i)), radius=0.2, facecolor=color,
-                                zorder=1, edgecolor='black')
-            circles.append(circle)
-            ax.add_patch(circle)
-            for j in range(agent.n_states):
-                texts.append(ax.text(j, i, int(agent.rewards[i, j]), zorder=2))
+    ax, agent, value_matrix, states, next_states = ax1, agent1, value_matrix1, states1, next_states1
+    im = ax.imshow(value_matrix, vmin=0, vmax=1, cmap='Oranges',
+                   animated=True)
+    im.set_zorder(0)
+    pos = states.astype(float)
+    step_size = 0
+    circles = []
+    texts = []
+    for i in range(agent.n_batch):
+        color = 'black' if i == 0 else 'gray'
+        circle = plt.Circle((states[i], y(i)), radius=0.2, facecolor=color,
+                            zorder=1, edgecolor='black')
+        circles.append(circle)
+        ax.add_patch(circle)
+        for j in range(agent.n_states):
+            texts.append(ax.text(j, i, int(agent.rewards[i, j]), zorder=2))
 
-        timestep_text = ax.text(.5, 0, 'timestep = {}'.format(0),
-                                 verticalalignment='bottom',
-                                 horizontalalignment='center',
-
-                                 transform=ax.transAxes, zorder=2)
+    timestep_text = ax.text(.5, 0, 'timestep = {}'.format(0),
+                            verticalalignment='bottom',
+                            horizontalalignment='center',
+                            transform=ax.transAxes, zorder=2)
 
 
     def updatefig(_):
-        global pos, states1, next_states1, step_size, agent1
+        global states1, next_states1, step_size, agent1
+        pos = [circle.center[0] for circle in circles]
         if agent1.timestep == agent1.max_timesteps:
             states1 = agent1.reset()
             next_states1 = states1
@@ -55,7 +55,8 @@ if __name__ == '__main__':
             time.sleep(1)
         else:
             if np.allclose(pos, next_states1):
-                actions, states1, next_states1, reward = agent1.step(states1)
+                assert np.allclose(states1, next_states1), (states1, next_states1)
+                actions, next_states1, reward = agent1.step(states1)
                 step_size = (next_states1 - states1) / 5
                 states1 = next_states1
                 im.set_array(agent1.value_matrix)

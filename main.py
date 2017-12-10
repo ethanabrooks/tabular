5d76b825aa826cf2c65997ed6bea8b2ea4d89e17
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-10 11:38:05 -0500

    added Animation class, but not using it. Animation1 currently works

--- main.py
+++ main.py
@@ -9,6 +9,52 @@ import algorithm
 SPEED = 1 / 20
 assert SPEED < 1
 
+
+class Animation:
+    def __init__(self, ax, agent, value_matrix, states):
+        self.agent = agent
+        self.im = ax.imshow(value_matrix, vmin=0, vmax=1, cmap='Oranges',
+                            animated=True)
+        self.im.set_zorder(0)
+        self.pos = states.astype(float)
+        self.step_size = 0
+        self.circles = []
+        self.texts = []
+        for i in range(agent.n_batch):
+            color = 'black' if i == 0 else 'gray'
+            circle = plt.Circle((states[i], y(i)), radius=0.2, facecolor=color,
+                                zorder=1, edgecolor='black')
+            circles.append(circle)
+            ax.add_patch(circle)
+            for j in range(agent.n_states):
+                texts.append(ax.text(j, i, int(agent.rewards[i, j]), zorder=2))
+
+        self.timestep_text = ax.text(.5, 0, 'timestep = {}'.format(0),
+                                     verticalalignment='bottom',
+                                     horizontalalignment='center',
+                                     transform=ax.transAxes, zorder=2)
+
+    def updatefig(self, _):
+        pos = [circle.center[0] for circle in circles]
+        if self.agent.timestep == self.agent.max_timesteps:
+            states = self.agent.reset()
+            self.next_states = states
+            pos = states.astype(float)
+            self.step_size = 0
+            time.sleep(1)
+        else:
+            if np.allclose(pos, self.next_states):
+                states = self.next_states
+                actions, self.next_states, reward = self.agent.step(states)
+                self.step_size = (self.next_states - states) * SPEED
+                im.set_array(self.agent.value_matrix)
+            pos += self.step_size
+        for i, j in enumerate(pos):
+            circles[i].center = (x(j), i)
+        timestep_text.set_text('timestep = {}'.format(self.agent.timestep))
+        return [im, timestep_text] + texts + circles
+
+
 if __name__ == '__main__':
     agent1, value_matrix1, states1, next_states1 = algorithm.init()
 
@@ -52,7 +98,10 @@ if __name__ == '__main__':
                                 transform=ax.transAxes, zorder=2)
         return im, texts, circles, pos, step_size, timestep_text
 
-    im, texts, circles, pos, step_size, timestep_text = init(ax1, agent1, value_matrix1, states1)
+
+    im, texts, circles, pos, step_size, timestep_text = init(ax1, agent1,
+                                                             value_matrix1,
+                                                             states1)
 
 
     def updatefig(_):
